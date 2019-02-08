896e84d6716a3926aa47fe0e7318361cd9cdf93f
Darren Ford <dford_au-reg@yahoo.com>
2019-02-08 15:50:20 +1100

    Initial Commit

---
+++ DSFFinderLabels/DSFFinderLabels.h
@@ -0,0 +1,19 @@
+//
+//  DSFFinderLabels.h
+//  DSFFinderLabels
+//
+//  Created by Darren Ford on 8/2/19.
+//  Copyright © 2019 Darren Ford. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DSFFinderLabels.
+FOUNDATION_EXPORT double DSFFinderLabelsVersionNumber;
+
+//! Project version string for DSFFinderLabels.
+FOUNDATION_EXPORT const unsigned char DSFFinderLabelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSFFinderLabels/PublicHeader.h>
+
+
